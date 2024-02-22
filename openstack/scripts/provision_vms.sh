8453c444cb184ace2257d239bc511332cfa7255f
Mattias <mattias@kockum.net>
2024-02-22 01:45:45 +0100

    stack creating well

--- openstack/scripts/provision_vms.sh
+++ openstack/scripts/provision_vms.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define stack name
-STACK_NAME="my-stack"
+STACK_NAME="project-stack"
 
 # Launch the Heat stack
 openstack stack create --template templates/hot_template.yaml --environment environments/environment.yaml $STACK_NAME
@@ -30,11 +30,14 @@ echo "Stack creation completed successfully."
 # Retrieve floating IP
 FLOATING_IP=$(openstack floating ip list --format json | jq -r '.[0]."Floating IP Address"')
 
-# Associate floating IP with VM1
-VM1_INSTANCE_ID=$(openstack server list --name VM1 --format json | jq -r '.[0].ID')
-openstack server add floating ip $VM1_INSTANCE_ID $FLOATING_IP
+echo "VM1 web interface can be accessed at: http://$FLOATING_IP"
 
-echo "Floating IP associated with VM1."
+
+# Associate floating IP with VM1
+#VM1_INSTANCE_ID=$(openstack server list --name VM1 --format json | jq -r '.[0].ID')
+#openstack server add floating ip $VM1_INSTANCE_ID $FLOATING_IP
+
+#echo "Floating IP associated with VM1."
 
 # Print information
-echo "VM1 web interface can be accessed at: http://$FLOATING_IP"
+#echo "VM1 web interface can be accessed at: http://$FLOATING_IP"
